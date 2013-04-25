11b448420013d8c42535fd5720e65d00381027f7
Miquel Serrabassa <>
2013-04-25 21:58:51 +0200

    El meu primer commit!

---
+++ src/com/whatsgroup/alkiria/messageserver/AlkiriaMessageServer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.whatsgroup.alkiria.messageserver;
+
+/**
+ *
+ * @author PC
+ */
+public class AlkiriaMessageServer {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
